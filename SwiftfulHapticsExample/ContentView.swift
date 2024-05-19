49c40d964bca0adf65866341ffa38c79392545fe
Nick <hello@swiftful-thinking.com>
2024-05-19 10:51:45 -0600

    Initial Commit

---
+++ SwiftfulHapticsExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulHapticsExample
+//
+//  Created by Nick Sarno on 5/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulHapticsExample/SwiftfulHapticsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulHapticsExampleApp.swift
+//  SwiftfulHapticsExample
+//
+//  Created by Nick Sarno on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulHapticsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
